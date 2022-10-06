d2668f21b3ac8b1d0686c371d5189d968bf80151
BuildTools <unconfigured@null.spigotmc.org>
2022-10-06 22:31:27 +0530

    checking if gitignore works

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from Library.bpp import Img
+from Library.colours import four_bpp
 
-samp = Img.new(600, 600, 8, 'INFO')
+samp = Img.new(600, 600, 4, 'INFO', colour= four_bpp.mindaro)
 samp.save('lol.bmp')
